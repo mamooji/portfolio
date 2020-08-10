ef07196a4447ff27a6ec3045ff3cd2d23bca27c7
Muhammad Mamooji <mamoojim@hotmail.com>
2020-08-10 18:41:13 -0400

    updated resume PDf/iframe stylings

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,18 +1,33 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
+import { Document, Page } from "react-pdf";
+import resume from "../../resumePDF/resumeLatex.pdf";
 
-class Resume extends Component {
-  render() {
-    return (
-      <Grid container direction="column">
-        <Grid item container>
-          <Grid item xs={false} sm={2} />
-          <Grid item xs={12} sm={8} style={{ marginBottom: "5em" }}></Grid>
-          <Grid item xs={false} sm={2} />
-        </Grid>
-      </Grid>
-    );
+const Resume = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
   }
-}
+  return (
+    <Grid container direction="column">
+      <Grid item container>
+        <Grid item xs={false} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <iframe
+            style={{
+              width: "100%",
+              height: "1000px",
+              border: "none",
+              padding: "1em",
+            }}
+            src={resume}
+          ></iframe>
+        </Grid>
+        <Grid item xs={false} sm={2} />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Resume;
